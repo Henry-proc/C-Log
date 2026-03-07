07798093f2361ef246b6cf099b7852e066580e4a
Henry Proctor <henry.rt.proctor@gmail.com>
2026-03-07 23:02:26 +0000

    initial commit creating the initial react files and creating first react element

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Climb from './Climb'
+
+function App() {
+  
+
+  return (
+    <>
+      <Climb name= "no one mournes the wicked"/> 
+      <Climb name= "burden of dreams"/> 
+    </>
+  )
+}
+
+export default App
---
+++ my-react-app/src/Climb.jsx
@@ -0,0 +1,11 @@
+
+export default function Climb({name}){
+    
+    return(
+        <div className="climb">
+            <h2>
+                {name}
+            </h2>
+        </div>
+    )
+}
